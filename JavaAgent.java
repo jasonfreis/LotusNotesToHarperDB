b7bfaaa5ee59e850a47a6fd919ec28eb56d950fd
Jason Freis <jason.freis@workflowstudios.com>
2020-06-14 09:34:29 -0500

    added JavaAgent.java and correct typo in README

---
+++ JavaAgent.java
@@ -0,0 +1,40 @@
+import lotus.domino.*;
+import java.io.*;
+import org.apache.commons.lang.*;
+import org.json.*;
+
+
+public class JavaAgent extends AgentBase {
+	public void NotesMain() {
+		try {
+			Session session = getSession();
+			
+			//Setup the database and view you want to export
+			Database db = session.getDatabase("", "TESTContacts.nsf");
+			View vw = db.getView("($People)");
+			
+			//Loop through the documents and write them to the file
+			Document doc = vw.getFirstDocument();
+			Document tmpdoc;
+			
+			//Create the file (change this to a directory that suits your needs)
+			String newFileName = "c:\\temp\\alldocs.csv";
+			BufferedWriter bw = new BufferedWriter(new FileWriter(newFileName));
+			bw.write("document" + "\n");
+			while (doc != null) {
+				String xml = doc.generateXML();
+				String JSON = org.json.XML.toJSONObject(xml).toString();
+				JSON = org.apache.commons.lang.StringUtils.right( JSON, JSON.length() - 12 );
+				JSON = org.apache.commons.lang.StringUtils.left(JSON, JSON.length() - 1);
+				JSON = JSON.replaceAll("\"", "\"\"");
+				bw.write("\"" + JSON + "\"" + "\n");
+				tmpdoc = vw.getNextDocument(doc);
+				doc.recycle();
+				doc = tmpdoc;
+			}
+			bw.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
